3d5bae53bf816d9102c36f1c6b8c04571b467de2
cgalleguillos-dev <galleguilloscristian2@gmail.com>
2023-06-21 14:43:05 -0400

    init

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            {
+              name: 'users', url: 'http://localhost:3001/graphql'
+            }
+          ]
+        })
+      }
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
