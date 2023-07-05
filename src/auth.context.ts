ed648fc1aa580f3d12a6f51ae0bcfa6a9b0cfbb7
cgalleguillos-dev <galleguilloscristian2@gmail.com>
2023-07-05 15:27:13 -0400

    fix: edit pathname

--- src/auth.context.ts
+++ src/auth.context.ts
@@ -5,6 +5,10 @@ export const authContext = async ({ req }) => {
 
     //verifica que no vaya a la ruta de login
     if (req.body.operationName === 'login') return;
+    if (req.body.operationName === 'signup') return;
+    if (req.body.operationName === 'GetMovies') return;
+    if (req.body.operationName === 'getMovieByID') return;
+    console.log(req.body.operationName);
     const token = req.headers.authorization || '';
     if (!token) throw new UnauthorizedException();
     const newToken = token.replace('Bearer ', '');
